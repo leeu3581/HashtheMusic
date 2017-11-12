dbdc76bc47dcdc0f5a99f939f00dd2faaaaa9835
leeu3581 <shlee9817@gmail.com>
2017-11-12 17:01:05 +0000

    Add files via upload

---
+++ HASHMUSIC.py
@@ -0,0 +1,60 @@
+import re, pprint, sys
+import time
+import tweepy
+from tweepy import StreamListener
+import spotipy
+import spotipy.util as util
+from tweepy import Stream
+
+#token = util.prompt_for_user_token(username="leeu3581", client_id='2e7dc5a56ef645daaa5920cb2aec074f',client_secret='bf874328eb96434da598b5716c78dcd6',redirect_uri='http://127.0.0.1:8080/callback/q')
+
+
+auth = tweepy.OAuthHandler('SfBiJ0tRqCVA9BygMxD2QSWpt','J39KhnJ4MYD2jTfYtjlCx1Mi8Zi1Evhq1ovNKzl85EY6uPCWIQ')
+auth.set_access_token('355494628-3aJoDVP8Ml8QYDwnGy91yhS0SwKZgwQ6MBpmJUhx','rgOW22QlYZNaUEABAPsGU2DUm2ZxqvIhBLhyunoYIu7Nk')
+
+twitter_api = tweepy.API(auth)
+
+results = tweepy.Cursor(twitter_api.search, q="#hashthemusic").items(1)
+
+#, geocode="51.5107,-0.1169,5km"
+#print stream.filter(track=['nice'])
+#results.replace("#hashthemusic", "")
+#for testresult in testresults:
+#    print testresult
+
+finalsong = ''
+for result in results:
+    finalsong = result.text
+    finalsong= re.sub('#hashthemusic', '', finalsong)
+    print finalsong
+
+    username = 'callooches'
+    playlist_id = '2R3CBX8QjXz9ooMzRHrN6I'
+    track_ids = '3A4Su2mc2azrtTt9uIVMwRlY'
+
+
+
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(username, scope, client_id='4eafe59b66604d8c975e3321d1fb66ff',client_secret='0904ebdfe829422e911aaa0af0245b14',redirect_uri='http://localhost:8888/callback')
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+    print results
+else:
+    print "Can't get token for", username
+
+#while True:
+#    try:
+#      tweet = results.next()
+#      finaltweet = tweet.text
+#      finaltweet = re.sub('#hashthemusic', '', finaltweet)
+#      print(finaltweet.text)
+
+# except tweepy.error.TweepError:
+#      print "Twitter rate limit, need to wait 15 min"
+#      time.sleep(60 * 16)
+#      continue
+ # except StopIteration:
+    #  break
